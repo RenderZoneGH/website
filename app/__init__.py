09429c3feefd8843007af52712f7043935bf3e86
pernydev <83672513+pernydev@users.noreply.github.com>
2022-12-27 01:41:56 +0200

    Add secure code

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ paypalrestsdk.configure({
 def before_request():
     if "admin" in request.path or "static" in request.path or "api" in request.path or "robots.txt" in request.path or "join" in request.path:
         return 
-    if not session.get("admin", False) or not (session.get("accesskey", "key") == env("cskey", "key")):
+    if not session.get("admin", False) or not (session.get("accesskey", "key") == env("cskey", "PJNijeoksnosjnwojn28ienojsniehj3nskns8n3u3nojwnj3n4ij3ijsiwpkwmpwm92kkmqpkw0ke9j3ij3ksns9enk2o")):
         return render_template('coming-soon.html', session=session, animation=a(request), nofooter=True)
 
 @flask.template_filter('strftime')
